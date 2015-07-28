46eac46563aeb8aa3d775cd7c2a204155e8af609
gh321123150 <417874293@qq.com>
2015-07-28 11:48:44 +0800

    fdsfsd

---
+++ Num/src/Test.java
@@ -0,0 +1,27 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		long microsPerDay = 60 * 60 * 1000l * 1000 * 24;  
+//		System.out.println(1.02+8.28);
+//		System.out.println(new BigDecimal("2.0").subtract(new BigDecimal("1.10")));// 0.9  
+//
+//		int a = 5;
+		List<String> list = new ArrayList<String>();
+		list.add("ddd");
+		list.add("eee");
+		list.add("aaa");
+		for (String val : list) {
+			System.out.println(val);
+		}
+	}
+
+}
